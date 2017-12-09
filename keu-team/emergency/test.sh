6374c8dca70bbbeacf5d98734ea21b81933925f3
UmaMoiseenko <>
2017-12-09 14:13:11 -0500

    testing text api

---
+++ keu-team/emergency/test.sh
@@ -0,0 +1,5 @@
+curl -X POST https://rest.messagebird.com/messages \
+-H 'Authorization: AccessKey zvnxWx0VyneBqXSFt0HUbwbVX' \
+-d "recipients=13479869282" \
+-d "originator=Uma" \
+-d "body=This is a test message from Eugene"
